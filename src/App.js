358c132239f17801867de73f4481927960ae0f80
cymoo <milkywayjc@hotmail.com>
2021-05-25 02:18:10 +0800

    init repo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import os from 'os'
+
+const electron = window.require('electron')
+const fs = window.require('fs')
 
 function App() {
+  const rv = fs.readdirSync(__dirname)
+  console.log(rv)
   return (
     <div className="App">
       <header className="App-header">
@@ -14,12 +20,18 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={(event) => {
+            event.preventDefault()
+            electron.ipcRenderer.invoke('open-new-page')
+          }}
         >
           Learn React
         </a>
+        <p>os: {os.hostname()}</p>
+        <p>user: {window.process.env.USER}</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
